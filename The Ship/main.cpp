88e8478700b36553966f7b72c0240f2615ff5ca2
Sean Perryman <se@nperryman.com>
2014-09-18 08:46:41 -0700

    Initial Commit

---
+++ The Ship/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  The Ship
+//
+//  Created by Sean Perryman on 9/18/14.
+//  Copyright (c) 2014 Sean Perryman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
