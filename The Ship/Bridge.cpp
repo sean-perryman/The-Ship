dda09c86ef877334911e63d476315acd7e67742e
Sean Perryman <se@nperryman.com>
2014-09-18 15:40:15 -0700

    Updated goals and the readme file

--- The Ship/Bridge.cpp
+++ The Ship/Bridge.cpp
@@ -12,6 +12,14 @@ using namespace std;
 
 bool Bridge() {
     /* Would like something here where the player has to figure out a combination to unlock the computer
-     to continue the ship on its voyage. Not sure how to do it just yet.*/
+     to continue the ship on its voyage. Not sure how to do it just yet.
+     
+     This ties into the medical suite where the player will have to search the captain's body for a notebook
+     containing the computer code to start the engines; as well as key that will let the player into the 
+     captains ready room.
+     
+     Initial thoughts are that the player won't even see the option to enter the ready room if they don't have
+     the key. Perhaps there they can find some sort of weapon or something that will let them fight the monster
+     that is in the crew quarters?*/
     return true;
 }
--- The Ship/MedicalSuite.cpp
+++ The Ship/MedicalSuite.cpp
@@ -11,5 +11,9 @@
 using namespace std;
 
 bool MedicalSuite() {
+    /* Here the player will see the only dead body in the game; the captain. He will have
+     to search the corpse for a notebook and a set of keys. The notebook will have the code
+     to start the engines, and the keys will unlock the captains ready room off of the bridge. 
+     */
     return true;
 }
